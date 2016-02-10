eab309f7f2e984ec435ae214a7102b494bc045b0
joncoop <joncoop1972@gmail.com>
2016-02-10 14:18:38 -0500

    Load json file with song info.

--- pytunes.py
+++ pytunes.py
@@ -3,5 +3,17 @@
 import tkinter
 import json
 
+file = 'music.json'
+
+def load():
+    with open(file, 'r') as f:
+        txt = f.read()
+
+    return json.loads(txt)
 
 
+def play(song_number):
+    pass
+
+if __name__ == '__main__':
+    library = load()
