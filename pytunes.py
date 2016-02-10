9e3c20f8d235305d88a06a682635ce68b9fd2190
joncoop <joncoop1972@gmail.com>
2016-02-10 14:49:08 -0500

    Made a crude tkinter app with play/pause

--- pytunes.py
+++ pytunes.py
@@ -1,33 +1,59 @@
 # Pytunes music player
-
+# Tkinter tutorial - http://www.python-course.eu/python_tkinter.php 
 
 # imports
 import json
 import pygame
-import tkinter
+from tkinter import *
+
 
 # initialize mixer
 pygame.mixer.init()
 
+
 # global config
-file = 'music.json'
-
-def load():
-    with open(file, 'r') as f:
-        txt = f.read()
-
-    return json.loads(txt)
+music_data = 'music.json'
 
 
-def play(song):
-    path = song['path']
-    pygame.mixer.music.load(path)
-    pygame.mixer.music.play()
-    
-def pause():
-    pygame.mixer.music.stop()
+class App:
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
 
-    
+        self.play = Button(frame, 
+                             text="Play",
+                             fg="green",
+                             command=self.play)
+
+        self.pause = Button(frame,
+                             text="Pause",
+                             fg="red",
+                             command=self.pause)
+
+        self.play.pack(side=LEFT)    
+        self.pause.pack(side=LEFT)
+
+        self.library = self.load(music_data)
+
+    def load(self, path):
+        with open(path, 'r') as f:
+            txt = f.read()
+
+        return json.loads(txt)
+                      
+    def play(self, song=None):
+        #path = song['path']
+        path = self.library[0]['path'] # for testin
+        
+        pygame.mixer.music.load(path)
+        pygame.mixer.music.play()
+        
+    def pause(self):
+        pygame.mixer.music.stop()
+
+        
 # main
 if __name__ == '__main__':
-    library = load()
+    root = Tk()
+    app = App(root)
+    root.mainloop()
